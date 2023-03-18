b14f28433de26155df814c47d7bbf3bd603feea3
Diego Cruz <diegoszcruz1001@gmail.com>
2023-03-17 23:24:32 -0300

    add routes

--- website/src/App.js
+++ website/src/App.js
@@ -1,8 +1,23 @@
+import { GlobalStyle } from "./globalStyles";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Hero Feature Content Carousel
+
+// pages
+import Home from "./pages/Home";
+import SignUp from './pages/SignupPage'
+import Pricing from './pages/PrincingPage'
+
 function App() {
   return (
-    <div className="App">
-      Teste
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/pricing' element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
